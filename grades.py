34ce422f178f3a7cdc52a63b43fd874cbef3703e
Dmitri Iouchtchenko <johnnyspoon@gmail.com>
2011-04-22 20:55:18 -0400

    Split the code up into some functions

--- grades.py
+++ grades.py
@@ -18,7 +18,46 @@ def parse_grade(grade):
 	except ValueError:
 		return ''
 
-# Get missing credentials.
+def fetch_grades(username, password, term):
+	"""Login to Quest, and get the grades for the given term."""
+	br = mechanize.Browser()
+
+	br.open('https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/SA/')
+	br.select_form('login')
+	br.form['userid'] = username
+	br.form['pwd'] = password
+	br.submit()
+
+	br.open('https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?Page=SSR_SSENRL_GRADE')
+	br.select_form('win0')
+	br.form.set_all_readonly(False)
+	br.form['SSR_DUMMY_RECV1$sels$0'] = [term]
+	br.form['ICAction'] = 'DERIVED_SSS_SCT_SSR_PB_GO'
+	br.submit()
+
+	resp = br.response().read()
+
+	br.close()
+
+	return resp
+
+def parse_response(resp):
+	soup = BeautifulSoup.BeautifulSoup(resp)
+
+	courses = [x.text for x in soup.findAll(attrs={'id': re.compile('CLS_LINK.+')})]
+	grades = [x.text for x in soup.findAll(attrs={'class': 'PABOLDTEXT'})]
+
+	return courses, grades
+
+def make_table(courses, grades):
+	pt = prettytable.PrettyTable(["Course", "Grade"])
+
+	for course, grade in zip(courses, grades):
+		pt.add_row([course, parse_grade(grade)])
+
+	return pt
+
+# Get any missing credentials.
 if not username:
 	username = raw_input('Quest username: ')
 
@@ -26,32 +65,6 @@ if not password:
 	password = getpass.getpass('Password for %s: ' % (username))
 
 # Go!
-br = mechanize.Browser()
-
-br.open('https://quest.pecs.uwaterloo.ca/psp/SS/ACADEMIC/SA/')
-br.select_form('login')
-br.form['userid'] = username
-br.form['pwd'] = password
-br.submit()
-
-br.open('https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/SA/c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?Page=SSR_SSENRL_GRADE')
-br.select_form('win0')
-br.form.set_all_readonly(False)
-br.form['SSR_DUMMY_RECV1$sels$0'] = ['2']
-br.form['ICAction'] = 'DERIVED_SSS_SCT_SSR_PB_GO'
-br.submit()
-
-resp = br.response().read()
-
-br.close()
-
-soup = BeautifulSoup.BeautifulSoup(resp)
-courses = [x.text for x in soup.findAll(attrs={'id': re.compile('CLS_LINK.+')})]
-grades = [x.text for x in soup.findAll(attrs={'class': 'PABOLDTEXT'})]
-
-pt = prettytable.PrettyTable(["Course", "Grade"])
-
-for course, grade in zip(courses, grades):
-	pt.add_row([course, parse_grade(grade)])
-
-pt.printt(header=False, border=False)
+resp = fetch_grades(username, password, '2')
+courses, grades = parse_response(resp)
+make_table(courses, grades).printt(header=False, border=False)
