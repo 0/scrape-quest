4db8b5e76b3a6b6d004076e6a0da21b90f12b40e
Dmitri Iouchtchenko <johnnyspoon@gmail.com>
2011-04-23 04:26:30 -0400

    Added some error handling and docstrings

--- scraper.py
+++ scraper.py
@@ -2,7 +2,26 @@ import BeautifulSoup
 import mechanize
 import re
 
+
+class InterfaceError(Exception):
+	"""
+	An exceptional turn of events when talking to Quest.
+	"""
+
+
+class LoginError(Exception):
+	"""
+	Failure to log in.
+	"""
+
+
 class QuestScraper(object):
+	"""
+	A scraper for Quest.
+
+	All methods raise InterfaceError if they encounter something unexpected.
+	"""
+
 	base_url = 'https://quest.pecs.uwaterloo.ca/psc/SS/ACADEMIC/SA/'
 
 	def __init__(self):
@@ -26,13 +45,39 @@ class QuestScraper(object):
 				text=re.compile('Sign out')))
 
 	def login(self, username, password):
+		"""
+		Attempt to create a new session for the given username.
+
+		Raises LoginError if Quest refuses the credentials.
+		"""
+
 		self.br.open(self.base_url + '?cmd=login')
 		self.br.select_form('login')
 		self.br.form['userid'] = username
 		self.br.form['pwd'] = password
 		self.br.submit()
 
+		resp = self.br.response().read()
+		soup = BeautifulSoup.BeautifulSoup(resp)
+
+		if 'errorCode' in self.br.geturl():
+			error_tags = soup.findAll(attrs={'class': 'PSERRORTEXT'})
+			if len(error_tags) == 1:
+				raise LoginError('Quest: %s' % (error_tags[0].text))
+			else:
+				raise InterfaceError('Could not determine error when logging in.')
+		elif self.is_logged_in():
+			return True
+		else:
+			raise InterfaceError('Unexpected result upon logging in.')
+
 	def fetch_grades(self, term):
+		"""
+		Fetch the grades for a given term.
+
+		term -- Specified as the radio button ID string of the desired term.
+		"""
+
 		self.br.open(self.base_url + 'c/SA_LEARNER_SERVICES.SSR_SSENRL_GRADE.GBL?Page=SSR_SSENRL_GRADE')
 		self.br.select_form('win0')
 		self.br.form.set_all_readonly(False)
@@ -45,5 +90,9 @@ class QuestScraper(object):
 
 		courses = [x.text for x in soup.findAll(attrs={'id': re.compile('CLS_LINK')})]
 		grades = [x.text for x in soup.findAll(attrs={'class': 'PABOLDTEXT'})]
+
+		if len(courses) != len(grades):
+			raise InterfaceError('Mismatch between number of courses (%d) and'
+					'number of grades (%d)' % (len(courses), len(grades)))
 
 		return courses, grades
